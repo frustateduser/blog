f7b9307012cac0b0e581277148adb85abd8901f5
frustateduser <koustubhbadshah07@outlook.com>
2026-01-24 01:57:30 +0530

    Reorganized removed most of template material's modified the main page added personalized links and corrected path for resources

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Koustubh Badshah';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Koustubh Badshah";
+export const SITE_DESCRIPTION =
+  "Cybersecurity and computer science blog by Koustubh Badshah sharing in-depth CTF writeups, Hack The Box walkthroughs, penetration testing methodologies, network security, Wi-Fi attacks, Linux systems, and real-world security projects. Learn, explore, and enhance your cybersecurity skills with practical insights and tutorials.";
